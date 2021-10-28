9fed334204f31f3dda0d4c02fc30aa9c8727572a
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2021-10-28 15:03:15 +0530

    added nav-bar and created interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'SkiNet';
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<IPagination>('https://localhost:5001/api/products?pageSize=50')
+      .subscribe(
+        (res: IPagination) => {
+          this.products = res.data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
