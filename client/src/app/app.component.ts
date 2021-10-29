caeed0f66bff5eaf41ee1289870a7b65c80e6fe1
Divyanshu Ranjan Srivastava <greatdrs@gmail.com>
2021-10-29 14:59:05 +0530

    created client and added initial look

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Skinet';
-  products: IProduct[];
+  title = 'SkiNet';
 
-  constructor(private http: HttpClient) {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.http
-      .get<IPagination>('https://localhost:5001/api/products?pageSize=50')
-      .subscribe(
-        (res: IPagination) => {
-          this.products = res.data;
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
   }
 }
